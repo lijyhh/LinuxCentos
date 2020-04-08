610b90f175f486d2f0a559c60ec7b91c6aff0c9b
lijyhh <1285507636@qq.com>
2020-04-08 22:31:04 +0800

    FirstCommit

--- git-acp.sh
+++ git-acp.sh
@@ -2,11 +2,5 @@
 git add .
 git commit -m $1
 git push -u origin master
-echo 'Study notes has finished.'
+echo 'Upload has finished.'
 
-cp ./Gvim_Linux/Linux学习笔记.md /c/Users/lijy花花/Desktop/Linux/LinuxCentos
-cd /c/Users/lijy花花/Desktop/Linux/LinuxCentos
-git add .
-git commit -m $1
-git push -u origin master
-echo 'Download notes has finished.'
